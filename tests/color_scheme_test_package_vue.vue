42b62f89f2ba173277ac963a87b933cc977889c1
Gerard Roche <gerardroche@users.noreply.github.com>
2022-08-20 19:44:40 +0100

    add vue test case

---
+++ tests/color_scheme_test_package_vue.vue
@@ -0,0 +1,15 @@
+<!-- COLOR SCHEME TEST "MonokaiFree/MonokaiFree.tmTheme" SKIP IF NOT "Vue Syntax Highlight/Vue Component"
+
+    https://packagecontrol.io/packages/Vue%20Syntax%20Highlight
+-->
+<template>
+<!-- ^ fg=#f92672 fs= -->
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: ['on'],
+})
+</script>
