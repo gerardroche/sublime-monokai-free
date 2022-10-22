7fc2db744566f30b3c95edb6b3e5b0e13670a3d6
Gerard Roche <gerardroche@users.noreply.github.com>
2022-10-22 12:51:22 +0100

    Add support for Vue Syntax Highlight

--- tests/color_scheme_test_package_vue.vue
+++ tests/color_scheme_test_package_vue.vue
@@ -2,14 +2,36 @@
 
     https://packagecontrol.io/packages/Vue%20Syntax%20Highlight
 -->
-<template>
-<!-- ^ fg=#f92672 fs= -->
-</template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-    props: ['on'],
+<script setup>
+// ^ fg=#f92672 fs=
+//      ^ fg=#a6e22e fs=
+defineProps({
+// ^ fg=#66d9ef fs=
+  on: Boolean,
+// ^^ fg=#f8f8f2 fs=
+//    ^ fg=#66d9ef fs=italic
 })
 </script>
+
+<template #title>
+<!--  ^ fg=#f92672aa fs= -->
+    <!--  ^^ fg=#fd971f fs= -->
+    <slot name="title" />
+    <!--  ^ fg=#a6e22e fs= -->
+
+    <div v-if="x" :href="x" :fizz="x">
+    <!-- ^^^^ fg=#66d9ef fs= -->
+    <!--          ^^ fg=#66d9ef fs= -->
+    <!--                    ^^ fg=#66d9ef fs= -->
+        fizz</div>
+    <div v-else :class="x">
+    <!-- ^^ fg=#66d9ef fs= -->
+    <!--        ^^ fg=#66d9ef fs= -->
+        buzz</div>
+
+        <template>
+<!--    ^^^^^^^^^^ fg=#f92672aa fs= -->
+        </template>
+<!--    ^^^^^^^^^^^ fg=#f92672aa fs= -->
+
+</template>
