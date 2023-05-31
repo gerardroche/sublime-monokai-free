04c8cd3f83fd6cd010d66af5a79c82a65daecd79
Gerard Roche <gerardroche@users.noreply.github.com>
2023-05-31 23:24:33 +0100

    Vue tests are only valid on ST4

--- tests/color_scheme_test_package_vue.vue
+++ tests/color_scheme_test_package_vue.vue
@@ -5,35 +5,35 @@ Vue Syntax Highlight
 https://packagecontrol.io/packages/Vue%20Syntax%20Highlight
 -->
 <script setup>
-// ^ fg=#f92672 fs=
-//      ^ fg=#a6e22e fs=
+// ^ fg=#f92672 fs= build>=4148
+//      ^ fg=#a6e22e fs= build>=4148
 defineProps({
-// ^ fg=#66d9ef fs=
+// ^ fg=#66d9ef fs= build>=4148
   on: Boolean,
-// ^^ fg=#f8f8f2 fs=
-//    ^ fg=#66d9ef fs=italic
+// ^^ fg=#f8f8f2 fs= build>=4148
+//    ^ fg=#66d9ef fs=italic build>=4148
 })
 </script>
 
 <template #title>
-<!--  ^ fg=#b32657 fs= -->
-    <!--  ^^ fg=#fd971f fs= -->
+<!--  ^ fg=#b32657 fs= build>=4148 -->
+    <!--  ^^ fg=#fd971f fs= build>=4148 -->
     <slot name="title" />
-    <!--  ^ fg=#a6e22e fs= -->
+    <!--  ^ fg=#a6e22e fs= build>=4148 -->
 
     <div v-if="x" :href="x" :fizz="x">
-    <!-- ^^^^ fg=#66d9ef fs= -->
-    <!--          ^^ fg=#66d9ef fs= -->
-    <!--                    ^^ fg=#66d9ef fs= -->
+    <!-- ^^^^ fg=#66d9ef fs= build>=4148 -->
+    <!--          ^^ fg=#66d9ef fs= build>=4148 -->
+    <!--                    ^^ fg=#66d9ef fs= build>=4148 -->
         fizz</div>
     <div v-else :class="x">
-    <!-- ^^ fg=#66d9ef fs= -->
-    <!--        ^^ fg=#66d9ef fs= -->
+    <!-- ^^ fg=#66d9ef fs= build>=4148 -->
+    <!--        ^^ fg=#66d9ef fs= build>=4148 -->
         buzz</div>
 
         <template>
-<!--    ^^^^^^^^^^ fg=#b32657 fs= -->
+<!--    ^^^^^^^^^^ fg=#b32657 fs= build>=4148 -->
         </template>
-<!--    ^^^^^^^^^^^ fg=#b32657 fs= -->
+<!--    ^^^^^^^^^^^ fg=#b32657 fs= build>=4148 -->
 
 </template>
